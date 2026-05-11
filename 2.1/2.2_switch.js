76c0d5bd6ca34133ce0865f9e01c582806ce7dee
kskozlovsky45-design <ks.kozlovsky45@gmail.com>
2026-05-11 14:08:17 +0300

    Add files via upload
    В лекциях конструкция switch объясняется только для проверки равенства переменной конкретным значениям. Поэтому в решении применён паттерн switch(true), где каждый case проверяет логическое условие и выполняется первая ветка, дающая true.

---
+++ 2.1/2.2_switch.js
@@ -0,0 +1,41 @@
+function manyChecks() {
+    let a = Math.floor(Math.random() * 20) + 1;
+    console.log(`a = ${a}`);
+
+    let result = "";
+
+    switch (true) {  
+        case a > 10:
+            result += "a больше чем 10,";
+            break;
+        default:
+            result += "a меньше или равно 10,";
+            if (a === 5) result += "особый случай";
+    }
+
+    switch (a) {
+        case 15:
+            result += " но a не равно 15";
+            break;
+    }
+
+    switch (true) {
+        case a > 5:
+            result += " a больше чем 5";
+            break;
+        default:
+            result += " a меньше или равно 5";
+    }
+
+    switch (a % 2) {
+        case 0:
+            result += " a нечетное";
+            break;
+        default:
+            result += " a четное";
+    }
+
+    console.log(result);
+}
+
+manyChecks();
